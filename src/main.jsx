fc389fd631627d5b1eb9ce2b79b824a4873efd4d
MatthewKehoeErgo <matthew.kehoe@ergotechnologygroup.com>
2026-05-13 12:18:21 +0100

    Fix for Git Browser issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./features/review-mode/styles/review-mode.css";
 import "./index.css";
@@ -10,10 +10,10 @@ import { AppReviewModeRoot } from "./review-mode/AppReviewModeRoot.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <AppReviewModeRoot>
         <App />
       </AppReviewModeRoot>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
